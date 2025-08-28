179b8e30b6cb5931a3a37df89388e053c9cac3ed
Branislav Stojkovic <96660978+branislavstojkovic70@users.noreply.github.com>
2025-08-28 14:39:53 +0200

    Added Migrations and LaunchSettings changed
    Launch changed port
    Migrations added

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //api/account
+    public class AccountController: ControllerBase
+    {
+
+        [HttpGet]
+        public string Hi()
+        {
+            return "Hi";
+        }
+
+    }
+}
